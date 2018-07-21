916caee7ec1f2a3813409c6c35abce556910d3eb
Fernando Rezk <fernandorezk@outlook.com>
2018-07-20 23:12:58 -0300

    Base project

---
+++ peopleware-service/src/main/java/br/com/rezk/peopleware/service/App.java
@@ -0,0 +1,13 @@
+package br.com.rezk.peopleware.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
